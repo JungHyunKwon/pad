c398b811261c8e2b65586bf30b18166a4606321f
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:14:16 +0900

    published

--- js/pad.js
+++ js/pad.js
@@ -2,42 +2,38 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
-	(function() {
-		'use strict';
+(function() {
+	'use strict';
 
-		/**
-		 * @name 숫자 확인
-		 * @since 2017-12-06
-		 * @param {*} value
-		 * @return {boolean}
-		 */
-		function _isNumeric(value) {
-			return typeof value === 'number' && !isNaN(value) && isFinite(value);
+	/**
+	 * @name 숫자 확인
+	 * @since 2017-12-06
+	 * @param {*} value
+	 * @return {boolean}
+	 */
+	function _isNumeric(value) {
+		return typeof value === 'number' && !isNaN(value) && isFinite(value);
+	}
+
+	/**
+	 * @name pad
+	 * @param {number} value
+	 * @return {number || string}
+	 * @since 2018-07-13
+	 */
+	window.pad = function(value) {
+		var result = NaN;
+		
+		//숫자이면서 0초과이면서 10미만일 때
+		if(_isNumeric(value)) {
+			//0초과이면서 10미만일 때
+			if(value > 0 && 10 > value) {
+				result = '0' + value;	
+			}else{
+				result = value;
+			}
 		}
 
-		/**
-		 * @name pad
-		 * @param {number} value
-		 * @return {number || string}
-		 * @since 2018-07-13
-		 */
-		window.pad = function(value) {
-			var result = NaN;
-			
-			//숫자이면서 0초과이면서 10미만일 때
-			if(_isNumeric(value)) {
-				//0초과이면서 10미만일 때
-				if(value > 0 && 10 > value) {
-					result = '0' + value;	
-				}else{
-					result = value;
-				}
-			}
-
-			return result;
-		};
-	})();
-}catch(e) {
-	console.error(e);
-}
+		return result;
+	};
+})();
