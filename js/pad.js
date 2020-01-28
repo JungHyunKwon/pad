31b66aa1b37ea795c15ef4aa75e72ae2626b9387
JungHyunKwon <kjh_3859@naver.com>
2020-01-28 20:14:04 +0900

    published

--- js/pad.js
+++ js/pad.js
@@ -11,27 +11,23 @@
 	 * @param {*} value
 	 * @return {boolean}
 	 */
-	function _isNumeric(value) {
+	function isNumeric(value) {
 		return typeof value === 'number' && !isNaN(value) && isFinite(value);
 	}
 
 	/**
 	 * @name pad
 	 * @param {number} value
-	 * @return {number || string}
+	 * @return {number}
 	 * @since 2018-07-13
 	 */
 	window.pad = function(value) {
 		var result = NaN;
 		
-		//숫자이면서 0초과이면서 10미만일 때
-		if(_isNumeric(value)) {
-			//0초과이면서 10미만일 때
-			if(value > 0 && 10 > value) {
-				result = '0' + value;	
-			}else{
-				result = value;
-			}
+		//숫자일 때
+		if(isNumeric(value)) {
+			//0 보다 크면서 10 보다 작을 때
+			result = (value > 0 && 10 > value) ? result = '0' + value : value;
 		}
 
 		return result;
